0b806e6aaa523b29d32278cac3129d6a2a35bd31
Crystal Zhang <crystalz16@nycstudents.net>
2024-09-30 23:07:47 -0400

    Update Zhang_Crystal_Lab_3.cpp

--- Zhang_Crystal_Lab_3.cpp
+++ Zhang_Crystal_Lab_3.cpp
@@ -9,24 +9,44 @@ using namespace std;
 
 int main() 
 {
-    cout << setfill ('#');
-    cout << setw (85) << "" << endl; //hashtag
+    cout << setfill ('#'); //fills all the empty spaces with hashtags
+    cout << setw (85) << "" << endl; //fills 85 characters with hashtags
 
-    cout << setfill (' ');
-    cout << "#" << setw (84) << "#" << endl;
-    cout << "#" << setw (84) << "#" << endl; 
-    cout << "#"" << setw (50) << "Ways to access the Task Manager on your Windows computer:" << setw (26) << "#" << endl; //start of body
-    cout << "#" << setw (84) << "#" << endl; 
-    cout << "#" << setw (58) << "Press the key combination Ctrl + Shift + Escape" << setw (25) << "#" << endl; //space and hashtag
-    cout << "#" << setw (84) << "#" << endl; 
-    cout << "#" << setw (82) << "Press the key combination Ctrl + Alt + Delete and select \"Task Manager\"" << setw (2) << "#" << endl; //space and hashtag
-    cout << "#" << setw (84) << "#" << endl; 
-    cout << "#" << setw (64) << "Type  in the Windows Start menu search \"Task Manager\"" << setw (20) << "#" << endl; //end of body
-    cout << "#" << setw (84) << "#" << endl;
-    cout << "#" << setw (84) << "#" << endl; //space and hashtag
+    cout << setfill (' '); //fills all empty spaces with blank spaces 
+    cout << "#" << setw (84) << "#" << endl; //sets a hashtag on the beginning, prints 84 blank spaces, then a hashatg on the end
+    cout << "#" << setw (84) << "#" << endl; //same above
 
-    cout << setfill ('#');                                                                         
-    cout << setw (85) << "" << endl; //hashtag
+    cout << "#" << setw (58) << "Ways to access the Task Manager on your Windows computer:" << setw (26) << "#" << endl; //prints a hashtag on the beginning, then 1 blank space, then the text (57 characters), then 25 blank spaces, and a hastag on the end 
+    cout << "#" << setw (84) << "#" << endl; //sets a hashtag on the beginning, prints 84 blank spaces, then a hashatg on the end
+    cout << "#" << setw (58) << "Press the key combination Ctrl + Shift + Escape" << setw (26) << "#" << endl; //prints a hashtag on the beginning, then 11 blank spaces, then the text (47 characters), then 35 blank spaces, then a hashtag on the end
+    cout << "#" << setw (84) << "#" << endl; //sets a hashtag on the beginning, prints 84 blank spaces, then a hashatg on the end
+    cout << "#" << setw (82) << "Press the key combination Ctrl + Alt + Delete and select \"Task Manager\"" << setw (2) << "#" << endl; //prints a hastag in the beginning, then 11 blank spaces, then the text (71 characters), then 1 blank space, and a hashtag on the end 
+    cout << "#" << setw (84) << "#" << endl; //sets a hashtag on the beginning, prints 84 blank spaces, then a hashatg on the end
+    cout << "#" << setw (64) << "Type  in the Windows Start menu search \"Task Manager\"" << setw (20) << "#" << endl; //prints a hashatg in the beginning, then 11 blank spaces, then the text (53 characters), then 19 blank spaces, then a hashtag on the end
+    
+    cout << "#" << setw (84) << "#" << endl; //sets a hashtag on the beginning, prints 84 blank spaces, then a hashatg on the end
+    cout << "#" << setw (84) << "#" << endl; //same above
+
+    cout << setfill ('#'); //fills all empty spaces with hashtags          
+    cout << setw (85) << "" << endl; //fills 85 characters with hashtags
+    
+    /* 
+    output: 
+    #####################################################################################
+    #                                                                                   #
+    #                                                                                   #
+    # Ways to access the Task Manager on your Windows computer:                         #
+    #                                                                                   #
+    #           Press the key combination Ctrl + Shift + Escape                         #
+    #                                                                                   #
+    #           Press the key combination Ctrl + Alt + Delete and select "Task Manager" #
+    #                                                                                   #
+    #           Type  in the Windows Start menu search "Task Manager"                   #
+    #                                                                                   #
+    #                                                                                   #
+    #####################################################################################
+    */
+    
 
     return 0; //return code
 
